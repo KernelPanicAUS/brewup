29ef4cf00bb7bb8cd1313f0adf0166ad9078b03f
Thomas Khalil <thomas@khalilt.com>
2016-03-10 09:48:21 +1100

    Brewup script

---
+++ brewup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+brewup_file=/usr/local/bin/brewup
+launch_daemon=~/Library/LaunchAgents/brewup.job.plist
+
+echo " --- Creating brew maintenance script at: $brewup_file"
+
+touch $brewup_file
+
+cat <<-END >> "$brewup_file"
+#!/bin/bash
+
+brew=/usr/local/bin/brew
+logger=/usr/bin/logger
+
+$brew update 2>&1  | $logger -t brewup.update
+$brew upgrade 2>&1 | $logger -t brewup.upgrade
+$brew cleanup 2>&1 | $logger -t brewup.cleanup
+END
+
+echo " --- Creating launch daemon at: $launch_daemon"
+
+touch $launch_daemon
+
+echo " --- Loading launch daemon $launch_daemon"
+
+cat <<-END >> "$launch_daemon"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>local.homebrew.maintenance</string>
+  <key>Program</key>
+  <string>/usr/local/bin/brewup</string>
+  <key>StartInterval</key>
+  <integer>10800</integer>
+</dict>
+</plist>
+END
+
+echo " --- Running brew maintenance now..."
+echo " --- Run: tail -f /var/log/system.log to monitor updates..."
